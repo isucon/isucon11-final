6571059d0382107374bfb7a117e17c09426ff174
yhattori <yhattori@yahoo-corp.jp>
2021-06-20 17:07:05 +0900

    [bench] 💬 fix comment line

--- benchmarker/fails/fails.go
+++ benchmarker/fails/fails.go
@@ -2,8 +2,8 @@ package fails
 
 import "github.com/isucon/isucandar/failure"
 
+// BenchmarkStepにAddErrorしていくエラー郡
 const (
-	// BenchmarkStepにAddErrorしていくエラー郡
 	// ErrCritical は即失格となるエラー
 	ErrCritical failure.StringCode = "critical-error"
 	// ErrApplication は正しいアプリケーションの挙動と異なるときのエラー。ある程度許容される。
