fa131c45ad7a866fc9b740050abcacb08fd4dd5f
Kazuhiro Karino <kkarino@yahoo-corp.jp>
2021-06-30 15:52:24 +0900

    implement course registration and search modal tentatively

---
+++ webapp/frontend/components/CalendarCell.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="
+      border
+      h-40
+      overflow-hidden
+      transition
+      cursor-pointer
+      duration-500
+      ease
+      hover:bg-primary-100
+    "
+  >
+    <div
+      class="
+        flex flex-col
+        h-40
+        mx-auto
+        xl:w-40
+        lg:w-30
+        md:w-30
+        sm:w-full
+        px-2
+        py-2
+        overflow-hidden
+      "
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
