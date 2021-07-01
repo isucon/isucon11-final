e463b738d86999f383ba47ad1902091bde5e45a3
h-kawase <h.kawase@w.ntt.com>
2021-07-01 17:28:33 +0900

    add card component

---
+++ webapp/frontend/components/common/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="py-10 px-8 bg-white shadow-lg">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Card',
+})
+</script>
