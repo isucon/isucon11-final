801f522a92ce7f6223a6a2d442c29141e386e4e0
h-kawase <h.kawase@w.ntt.com>
2021-07-01 17:27:51 +0900

    load common components automatically

--- webapp/frontend/nuxt.config.js
+++ webapp/frontend/nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: ['~/components/', '~/components/common'],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
