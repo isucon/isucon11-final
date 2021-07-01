8ca70b10b395c83c871fa80c96aa0b794de14e7a
h-kawase <h.kawase@w.ntt.com>
2021-07-01 17:28:52 +0900

    add tab component

---
+++ webapp/frontend/components/common/Accordion.vue
@@ -0,0 +1,36 @@
+<template>
+  <card>
+    <div class="flex flex-col justify-between leading-normal">
+      <a
+        href="#"
+        class="p-4 block bg-white no-underline text-black"
+        @click.prevent="toggle"
+      >
+        <slot name="header" />
+        <div v-show="isOpen" class="p-2"><slot /></div>
+        <span v-show="!isOpen" class="text-primary-500"
+          >&#9660; すべて開く</span
+        >
+        <span v-show="isOpen" class="text-primary-500">&#9650; 閉じる</span>
+      </a>
+    </div>
+  </card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Accordion',
+  data(): { isOpen: boolean } {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    },
+  },
+})
+</script>
