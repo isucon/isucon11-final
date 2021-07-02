9c93286277990d504189904d39201a12b350dcd9
Kazuhiro Karino <kkarino@yahoo-corp.jp>
2021-07-02 16:30:20 +0900

    change the store of username from vuex to localstorage

--- webapp/frontend/middleware/is_loggedin.ts
+++ webapp/frontend/middleware/is_loggedin.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@nuxt/types'
 
 const isLoggedIn: Middleware = async (context) => {
-  if (!context.$cookies.get('session')) {
+  if (!context.$cookies.get('session') || !localStorage.getItem('user')) {
     return context.redirect('/')
   }
 }
