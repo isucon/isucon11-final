acc6fc3ec8e80e5473b7bd0a7ea7ad0111b480bc
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-07-13 17:49:42 +0900

    :recycle:refactor RegisterScores

--- benchmarker/api/grade.go
+++ benchmarker/api/grade.go
@@ -1,28 +1,33 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/isucon/isucandar/agent"
+	"github.com/isucon/isucandar/failure"
+	"github.com/isucon/isucon11-final/benchmarker/fails"
 )
 
-type registerGradeRequest struct {
-	UserID string `json:"user_id"`
-	Grade  uint32 `json:"grade"`
+type RegisterScoresRequestContent struct {
+	UserCode string `json:"user_code"`
+	Score    int    `json:"score"`
 }
 
-func RegisterGrades(ctx context.Context, a *agent.Agent, courseID, userID string, grade uint32) error {
-	req := &registerGradeRequest{
-		UserID: userID,
-		Grade:  grade,
-	}
-
-	_, err := apiRequest(ctx, a, http.MethodPost, fmt.Sprintf("/api/courses/%s/grades", courseID), req, nil, []int{http.StatusOK})
+func RegisterScores(ctx context.Context, a *agent.Agent, courseID, classID string, scores []RegisterScoresRequestContent) (*http.Response, error) {
+	body, err := json.Marshal(scores)
 	if err != nil {
-		return err
+		return nil, failure.NewError(fails.ErrCritical, err)
+	}
+	path := fmt.Sprintf("/api/courses/%s/classes/%s/assignments", courseID, classID)
+
+	req, err := a.NewRequest(http.MethodPost, path, bytes.NewReader(body))
+	if err != nil {
+		return nil, failure.NewError(fails.ErrCritical, err)
 	}
 
-	return nil
+	return a.Do(ctx, req)
 }
--- benchmarker/go.mod
+++ benchmarker/go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/isucon/isucandar v0.0.0-20210516110136-4274ebbe6701
 	github.com/isucon/isucon10-portal v0.0.0-20201008112716-8c0b637e1bd8
+	github.com/pborman/uuid v1.2.1
 )
