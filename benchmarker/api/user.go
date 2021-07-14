e62e5be854d04e54b3f5d67817270fa92c495c5a
yhattori <yhattori@yahoo-corp.jp>
2021-07-14 21:17:20 +0900

    ♻️  [bench] fix by staticcheck

--- benchmarker/api/user.go
+++ benchmarker/api/user.go
@@ -61,6 +61,9 @@ func RegisterCourses(ctx context.Context, a *agent.Agent, courses []RegisterCour
 	path := "/api/users/me/courses"
 
 	req, err := a.POST(path, bytes.NewReader(body))
+	if err != nil {
+		return nil, failure.NewError(fails.ErrCritical, err)
+	}
 
 	return a.Do(ctx, req)
 }
