b4a9b208f7fe600d21ffa071116243da2ecfe401
h-kawase <h.kawase@w.ntt.com>
2021-07-15 16:10:53 +0900

    modify components, middleware, and asyncData in course page

--- webapp/frontend/pages/courses/_id.vue
+++ webapp/frontend/pages/courses/_id.vue
@@ -10,7 +10,7 @@
           ]"
         >
           <template slot="announcements">
-            <Announcement
+            <AnnouncementCard
               v-for="(announcement, index) in announcements"
               :key="announcement.id"
               :announcement="announcement"
@@ -18,11 +18,11 @@
             />
           </template>
           <template slot="classworks">
-            <Classwork
-              v-for="classwork in classworks"
-              :key="classwork.id"
+            <ClassInfoCard
+              v-for="cls in classes"
+              :key="cls.id"
               :course="course"
-              :classwork="classwork"
+              :classinfo="cls"
             />
           </template>
         </tabs>
@@ -33,69 +33,59 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import {
-  Course,
-  Announcement,
-  ClassInfo,
-  Classwork,
-  Document,
-  Assignment,
-} from '~/types/courses'
+import { Course, Announcement, ClassInfo } from '~/types/courses'
+import Card from '~/components/common/Card.vue'
+import AnnouncementCard from '~/components/Announcement.vue'
+import ClassInfoCard from '~/components/ClassInfo.vue'
 
 type CourseData = {
   course: Course
   announcements: Array<Announcement>
-  classworks: Array<Classwork>
+  classes: Array<ClassInfo>
 }
 
+// TODO: announcement周りは#166~#168あたりの改修が済み次第
+
 export default Vue.extend({
+  components: {
+    Card,
+    AnnouncementCard,
+    ClassInfoCard,
+  },
+  middleware: 'is_loggedin',
   async asyncData({ params, $axios }): Promise<CourseData> {
-    const [course, announcements, documents, assignments, classInfo] =
-      await Promise.all([
-        $axios.$get(`/api/courses/${params.id}`),
-        // $axios.$get(`/api/courses/${params.id}/announcements`), // not implemented
-        // tentative
-        (() => {
-          const announcements: Array<Announcement> = [
-            {
-              id: '01234567-89ab-cdef-0010-000000000001',
-              courseName: '微分積分基礎',
-              title: 'The third class will be cancelled',
-              createdAt: new Date(1625573684000).toLocaleString(),
-            },
-            {
-              id: '01234567-89ab-cdef-0010-000000000002',
-              courseName: '微分積分基礎',
-              title: 'Comments for your assignments',
-              createdAt: new Date(1625573684000).toLocaleString(),
-            },
-          ]
-          return announcements
-        })(),
-        $axios.$get(`/api/courses/${params.id}/documents`),
-        $axios.$get(`/api/courses/${params.id}/assignments`),
-        $axios.$get(`/api/courses/${params.id}/classes`),
-      ])
+    const [course, classes, announcements] = await Promise.all([
+      $axios.$get(`/api/syllabus/${params.id}`),
+      $axios.$get(`/api/courses/${params.id}/classes`),
+      // $axios.$get(`/api/announcements?course_id=${params.id}`), // not implemented
+      // tentative
+      (() => {
+        const announcements: Array<Announcement> = [
+          {
+            id: '01234567-89ab-cdef-0010-000000000001',
+            courseName: '微分積分基礎',
+            title: 'The third class will be cancelled',
+            createdAt: new Date(1625573684000).toLocaleString(),
+          },
+          {
+            id: '01234567-89ab-cdef-0010-000000000002',
+            courseName: '微分積分基礎',
+            title: 'Comments for your assignments',
+            createdAt: new Date(1625573684000).toLocaleString(),
+          },
+        ]
+        return announcements
+      })(),
+    ])
     // api is not implemented
     // const announcements = announcements.map((item: Announcement) => {
     //   item.createdAt = new Date(item.createdAt).toLocaleString()
     //   return item
     // })
-    const classworks: Array<Classwork> = classInfo.map((cls: ClassInfo) => {
-      return {
-        ...cls,
-        documents: documents.filter(
-          (item: Document) => item.classId === cls.id
-        ),
-        assignments: assignments.filter(
-          (item: Assignment) => item.classId === cls.id
-        ),
-      }
-    })
     return {
       course,
       announcements,
-      classworks,
+      classes,
     }
   },
   data(): CourseData | undefined {
