95beab94cef75153b284783eb8bca6add7fe0ff2
Hosshii <sao_heath6147.wistre@icloud.com>
2021-07-18 14:24:33 +0900

    update user

--- benchmarker/model/user.go
+++ benchmarker/model/user.go
@@ -15,7 +15,82 @@ type Student struct {
 	*UserAccount
 	Agent *agent.Agent
 
+	registeredCourses     []*Course
+	hasUnreadAnnouncement bool
+	announcements         AnnouncementDeque
+	unreadAnnouncements   AnnouncementDeque
+
 	rmu sync.RWMutex
+}
+
+func NewStudent(id, rawPW string) *Student {
+	a, _ := agent.NewAgent()
+	return &Student{
+		UserAccount: &UserAccount{
+			ID:          id,
+			RawPassword: rawPW,
+		},
+		Agent:             a,
+		registeredCourses: make([]*Course, 0),
+		rmu:               sync.RWMutex{},
+	}
+}
+
+func (s *Student) RegisteredCoursesCount() int {
+	s.rmu.RLock()
+	defer s.rmu.RUnlock()
+
+	return len(s.registeredCourses)
+}
+
+func (s *Student) AddCourse(course *Course) {
+	s.rmu.Lock()
+	defer s.rmu.Unlock()
+
+	s.registeredCourses = append(s.registeredCourses, course)
+}
+
+func (s *Student) HasUnreadAnnouncement() bool {
+	s.rmu.RLock()
+	defer s.rmu.RUnlock()
+
+	return s.hasUnreadAnnouncement
+}
+
+func (s *Student) UnreadAnnouncements() []*Announcement {
+	s.rmu.RLock()
+	defer s.rmu.RUnlock()
+
+	// dequeでもmutex取ってるけどいらないかも知れない
+	return s.unreadAnnouncements.Items()
+}
+
+func (s *Student) PopOldestUnreadAnnouncements() *Announcement {
+	s.rmu.RLock()
+	defer s.rmu.RUnlock()
+
+	// TODO: ここでpubsubとかで課題提出workerにおくってもいいかもしれない
+	// dequeでもmutex取ってるけどいらないかも知れない
+	return s.unreadAnnouncements.PopFront()
+}
+
+func (s *Student) PushOldestUnreadAnnouncements(a *Announcement) {
+	s.rmu.Lock()
+	defer s.rmu.Unlock()
+
+	s.unreadAnnouncements.PushFront(a)
+}
+
+func (s *Student) PushLatestUnreadAnnouncements(a *Announcement) {
+	s.rmu.Lock()
+	defer s.rmu.Unlock()
+
+	s.unreadAnnouncements.PushBack(a)
+}
+
+func (s *Student) RemoveUnreadAnnouncement(id string) {
+	s.rmu.Lock()
+
 }
 
 type Faculty struct {
@@ -31,17 +106,5 @@ func NewFaculty(id, rawPW string) *Faculty {
 			RawPassword: rawPW,
 		},
 		Agent: a,
-	}
-}
-
-func NewStudent(id, rawPW string) *Student {
-	a, _ := agent.NewAgent()
-	return &Student{
-		UserAccount: &UserAccount{
-			ID:          id,
-			RawPassword: rawPW,
-		},
-		Agent: a,
-		rmu:   sync.RWMutex{},
 	}
 }
