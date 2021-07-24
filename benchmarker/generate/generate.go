69bfd410f9d42110f73af2c060b752ab5221ceb6
Hosshii <sao_heath6147.wistre@icloud.com>
2021-07-24 19:48:35 +0900

    add timer

--- benchmarker/generate/generate.go
+++ benchmarker/generate/generate.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/isucon/isucon11-final/benchmarker/model"
 )
+
+var (
+	once          sync.Once
+	timeGenerator *timer
+)
+
+func init() {
+	newTimer()
+}
 
 func Course() *model.Course {
 	randInt := rand.Intn(100)
@@ -17,4 +28,29 @@ func Submission() *model.Submission {
 	title := "test title"
 	data := []byte{1, 2, 3}
 	return model.NewSubmission(title, data)
+}
+
+type timer struct {
+	base  int64 // unix time
+	count int64
+
+	mu sync.Mutex
+}
+
+func newTimer() {
+	once.Do(func() {
+		timeGenerator = &timer{
+			base:  time.Now().Unix(),
+			count: 0,
+			mu:    sync.Mutex{},
+		}
+	})
+}
+
+func GenTime() int64 {
+	timeGenerator.mu.Lock()
+	defer timeGenerator.mu.Unlock()
+
+	timeGenerator.count++
+	return timeGenerator.base + timeGenerator.count
 }
