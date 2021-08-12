9f86b7b9e4045d64e9e5e4fee544071fe530cadd
Kazuhiro Karino <kkarino@yahoo-corp.jp>
2021-08-12 17:35:13 +0900

    delete deprecated option

--- webapp/frontend/nuxt.config.js
+++ webapp/frontend/nuxt.config.js
@@ -2,8 +2,6 @@ const config = {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  mode: 'spa',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'isucholar',
