abdec32fbfde9aff75119144b3114fde12c43b4d
TakahashiKazuya <kaz.t2497.bz@gmail.com>
2021-08-16 11:49:09 +0900

    ➕ add submitted assignments to class model

--- benchmarker/model/class.go
+++ benchmarker/model/class.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"crypto/md5"
+	"sync"
+)
+
 type ClassParam struct {
 	Title     string
 	Desc      string
@@ -9,12 +14,38 @@ type ClassParam struct {
 
 type Class struct {
 	*ClassParam
-	ID string
+	ID                   string
+	submittedAssignments map[string][16]byte // 学籍番号 -> 課題ファイルhash値
+	rmu                  sync.RWMutex
 }
 
 func NewClass(id string, param *ClassParam) *Class {
 	return &Class{
-		ClassParam: param,
-		ID:         id,
+		ClassParam:           param,
+		ID:                   id,
+		submittedAssignments: make(map[string][16]byte),
+		rmu:                  sync.RWMutex{},
 	}
+}
+
+func (c *Class) AddSubmittedAssignment(studentCode string, data []byte) {
+	c.rmu.Lock()
+	defer c.rmu.Unlock()
+
+	c.submittedAssignments[studentCode] = md5.Sum(data)
+}
+
+func (c *Class) GetAssignmentHash(studentCode string) ([16]byte, bool) {
+	c.rmu.RLock()
+	defer c.rmu.RUnlock()
+
+	hash, exists := c.submittedAssignments[studentCode]
+	return hash, exists
+}
+
+func (c *Class) GetSubmittedCount() int {
+	c.rmu.RLock()
+	defer c.rmu.RUnlock()
+
+	return len(c.submittedAssignments)
 }
