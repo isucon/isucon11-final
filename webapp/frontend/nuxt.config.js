aac48694e58d6f75394241c1325dc8be01daf3d1
Kazuhiro Karino <kkarino@yahoo-corp.jp>
2021-08-21 18:15:06 +0900

    remove duplicate configuration

--- webapp/frontend/nuxt.config.js
+++ webapp/frontend/nuxt.config.js
@@ -61,14 +61,8 @@ const config = {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     filenames: {
-      app: ({ isDev, isModern }) =>
-        isDev
-          ? `[name]${isModern ? '.modern' : ''}.js`
-          : `[name]${isModern ? '.modern' : ''}.js`,
-      chunk: ({ isDev, isModern }) =>
-        isDev
-          ? `[name]${isModern ? '.modern' : ''}.js`
-          : `[name]${isModern ? '.modern' : ''}.js`,
+      app: ({ isModern }) => `[name]${isModern ? '.modern' : ''}.js`,
+      chunk: ({ isModern }) => `[name]${isModern ? '.modern' : ''}.js`,
       css: ({ isDev }) => (isDev ? '[name].css' : 'css/[name].css'),
       img: ({ isDev }) => (isDev ? '[path][name].[ext]' : 'img/[name].[ext]'),
       font: ({ isDev }) =>
