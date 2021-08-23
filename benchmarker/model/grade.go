656df527453d43c0a7df246b1c89f71f4c06e9c3
Hosshii <sao_heath6147.wistre@icloud.com>
2021-08-23 23:29:55 +0900

    define validate grade type

--- benchmarker/model/grade.go
+++ benchmarker/model/grade.go
@@ -4,11 +4,10 @@ package model
 type SimpleCourseResult struct {
 	Name        string // course name
 	Code        string // course code
-	ClassScores []*ClassScore
+	ClassScores []*SimpleClassScore
 }
 
-func NewSimpleCourseResult(name, code string, classScores []*ClassScore) *SimpleCourseResult {
-
+func NewSimpleCourseResult(name, code string, classScores []*SimpleClassScore) *SimpleCourseResult {
 	return &SimpleCourseResult{
 		Name:        name,
 		Code:        code,
@@ -17,7 +16,7 @@ func NewSimpleCourseResult(name, code string, classScores []*ClassScore) *Simple
 
 }
 
-type ClassScore struct {
+type SimpleClassScore struct {
 	// 上3つの情報はclassから取得できるので無くてもいいかもしれない
 	ClassID string
 	Title   string
@@ -26,11 +25,76 @@ type ClassScore struct {
 	Score int // 0 - 100点
 }
 
-func NewClassScore(class *Class, score int) *ClassScore {
-	return &ClassScore{
+func NewSimpleClassScore(class *Class, score int) *SimpleClassScore {
+	return &SimpleClassScore{
 		ClassID: class.ID,
 		Title:   class.Title,
 		Part:    class.Part,
 		Score:   score,
+	}
+}
+
+type GradeRes struct {
+	Summary       Summary
+	CourseResults map[string]*CourseResult
+}
+
+func NewGradeRes(summary Summary, courseResults map[string]*CourseResult) GradeRes {
+	return GradeRes{
+		Summary:       summary,
+		CourseResults: courseResults,
+	}
+}
+
+type Summary struct {
+	Credits   int
+	GPT       float64
+	GptTScore float64 // 偏差値
+	GptAvg    float64 // 平均値
+	GptMax    float64 // 最大値
+	GptMin    float64 // 最小値
+}
+
+type CourseResult struct {
+	Name             string
+	Code             string
+	TotalScore       int
+	TotalScoreTScore float64 // 偏差値
+	TotalScoreAvg    float64 // 平均値
+	TotalScoreMax    int     // 最大値
+	TotalScoreMin    int     // 最小値
+	ClassScores      []*ClassScore
+}
+
+func NewCourseResult(Name, Code string, TotalScore, TotalScoreMax, TotalScoreMin int, TotalScoreTScore, TotalScoreAvg float64, ClassScores []*ClassScore) *CourseResult {
+	return &CourseResult{
+		Name,
+		Code,
+		TotalScore,
+		TotalScoreTScore,
+		TotalScoreAvg,
+		TotalScoreMax,
+		TotalScoreMin,
+		ClassScores,
+	}
+}
+
+type ClassScore struct {
+	// 上3つの情報はclassから取得できるので無くてもいいかもしれない
+	ClassID string
+	Title   string
+	Part    uint8
+
+	Score          int // 0 - 100点
+	SubmitterCount int
+}
+
+func NewClassScore(class *Class, score, submitterCount int) *ClassScore {
+	return &ClassScore{
+		ClassID:        class.ID,
+		Title:          class.Title,
+		Part:           class.Part,
+		Score:          score,
+		SubmitterCount: submitterCount,
 	}
 }
