3c1872e25684e0fe563e1ed2e494462aebceac47
yhattori <yhattori@yahoo-corp.jp>
2021-08-24 20:10:17 +0900

    [bench] convert admin logger to debug logger

--- benchmarker/scenario/logger.go
+++ benchmarker/scenario/logger.go
@@ -1,6 +1,7 @@
 package scenario
 
 import (
+	"io"
 	"log"
 	"os"
 )
@@ -10,9 +11,16 @@ var (
 	ContestantLogger *log.Logger
 	// 運営だけが見れる内容を書くロガー
 	AdminLogger *log.Logger
+	// デバッグ用で仕込んでいるロガー
+	DebugLogger *log.Logger
 )
 
 func init() {
 	ContestantLogger = log.New(os.Stdout, "", log.Lmicroseconds)
 	AdminLogger = log.New(os.Stderr, "", log.Lmicroseconds)
+	DebugLogger = log.New(os.Stderr, "", log.Lmicroseconds)
+}
+
+func SilenceDebugLog() {
+	DebugLogger = log.New(io.Discard, "", log.Lmicroseconds)
 }
