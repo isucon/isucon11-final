3f2d4dd2ba3c41fd44f5fbc81a22b396b30832a1
yhattori <yhattori@yahoo-corp.jp>
2021-08-24 13:15:01 +0900

    [bench] add course validation

--- benchmarker/scenario/validation.go
+++ benchmarker/scenario/validation.go
@@ -2,8 +2,12 @@ package scenario
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/isucon/isucandar"
+	"github.com/isucon/isucandar/failure"
+	"github.com/isucon/isucon11-final/benchmarker/api"
+	"github.com/isucon/isucon11-final/benchmarker/fails"
 )
 
 func (s *Scenario) Validation(ctx context.Context, step *isucandar.BenchmarkStep) error {
@@ -24,7 +28,63 @@ func (s *Scenario) validateAnnouncements(ctx context.Context, step *isucandar.Be
 }
 
 func (s *Scenario) validateCourses(ctx context.Context, step *isucandar.BenchmarkStep) {
+	errNotMatchCount := failure.NewError(fails.ErrCritical, fmt.Errorf("最終検証にて登録されている Course の個数が一致しませんでした"))
+	errNotMatch := failure.NewError(fails.ErrCritical, fmt.Errorf("最終検証にて存在しないはずの Course が見つかりました"))
 
+	students := s.ActiveStudents()
+	expectCourses := s.Courses()
+
+	if len(students) == 0 || len(expectCourses) == 0 {
+		return
+	}
+
+	// searchAPIを叩くユーザ
+	student := students[0]
+
+	_, err := LoginAction(ctx, student.Agent, student.UserAccount)
+	if err != nil {
+		step.AddError(failure.NewError(fails.ErrCritical, err))
+		return
+	}
+
+	var actuals []*api.GetCourseDetailResponse
+	// 空検索パラメータで全部ページング → コースをすべて集める
+	nextPathParam := "/api/syllabus"
+	for nextPathParam != "" {
+		hres, res, err := SearchCourseAction(ctx, student.Agent, nil, nextPathParam)
+		if err != nil {
+			step.AddError(failure.NewError(fails.ErrCritical, err))
+			return
+		}
+		for _, c := range res {
+			actuals = append(actuals, c)
+		}
+
+		_, nextPathParam = parseLinkHeader(hres)
+	}
+
+	if len(expectCourses) != len(actuals) {
+		step.AddError(errNotMatchCount)
+		return
+	}
+
+	// 中身の検証はLoadでしているので省略
+	for _, actual := range actuals {
+		expect := expectCourses[actual.ID.String()]
+
+		if !AssertEqual("course ID", expect.ID, actual.ID.String()) ||
+			!AssertEqual("course Type", api.CourseType(expect.Type), actual.Type) ||
+			!AssertEqual("course Credit", uint8(expect.Credit), actual.Credit) ||
+			!AssertEqual("course Teacher", expect.Teacher().Name, actual.Teacher) ||
+			// webappは1-6, benchは0-5
+			!AssertEqual("course Period", uint8(expect.Period+1), actual.Period) ||
+			// webappはMonday..., benchは0-6
+			!AssertEqual("course DayOfWeek", api.DayOfWeekTable[expect.DayOfWeek], actual.DayOfWeek) ||
+			!AssertEqual("course Keywords", expect.Keywords, actual.Keywords) ||
+			!AssertEqual("course Description", expect.Description, actual.Description) {
+			step.AddError(errNotMatch)
+		}
+	}
 	return
 }
 
