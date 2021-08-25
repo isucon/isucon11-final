5a5fef3fa40c6f29d56a28435a98f36ff4cbe2ea
h-kawase <h.kawase@w.ntt.com>
2021-08-25 16:30:33 +0900

    rename toppage for teacher

--- benchmarker/scenario/assets.go
+++ benchmarker/scenario/assets.go
@@ -5,14 +5,14 @@ var (
 		"": "55920142e0aadbaa284119f0c7ed599c",
 		"/200.html": "55920142e0aadbaa284119f0c7ed599c",
 		"/README.md": "5034cfd9edf3fa7d7d23589ecb2488f9",
-		"/_nuxt/app.js": "95c4ef8fa0593826dff32daa6a284bb1",
+		"/_nuxt/app.js": "61ac29ee11cf478ef97b5d50426debed",
 		"/_nuxt/css/app.css": "bb0bba79d66a56f2dcaddcc2900ec681",
 		"/_nuxt/runtime.js": "c51ffc86f28b62f78d641758e2dab72e",
 		"/announce": "55920142e0aadbaa284119f0c7ed599c",
-		"/facultypage": "55920142e0aadbaa284119f0c7ed599c",
 		"/favicon.ico": "ddf2e14a825fc1fff4b1d5f0cb48acda",
 		"/grade": "55920142e0aadbaa284119f0c7ed599c",
 		"/mypage": "55920142e0aadbaa284119f0c7ed599c",
 		"/register": "55920142e0aadbaa284119f0c7ed599c",
+		"/teacherpage": "55920142e0aadbaa284119f0c7ed599c",
 	}
 )
