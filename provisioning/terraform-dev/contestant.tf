cf5466ffadd052740c9e5a38341037bae6491dc1
takonomura <takonomura@users.noreply.github.com>
2021-08-29 22:20:52 +0900

    Add dev instances

--- provisioning/terraform-dev/contestant.tf
+++ provisioning/terraform-dev/contestant.tf
@@ -16,12 +16,17 @@
 
 variable "contestant_names" {
   type    = list(string)
-  default = ["takonomura", "temma", "hosshii", "buchy", "oribe", "eiya", "kanata", "hattori", "takahashi", "eagletmt", "sapphi_red"]
+  default = ["takonomura", "temma", "hosshii", "buchy", "oribe", "eiya", "kanata", "hattori", "takahashi", "eagletmt", "sapphi_red", "sorah"]
 }
 
 variable "two_instance_contestant_names" {
   type    = list(string)
-  default = ["sapphi_red"]
+  default = ["sapphi_red", "temma", "sorah"]
+}
+
+variable "three_instance_contestant_names" {
+  type    = list(string)
+  default = ["sorah", "sapphi_red"]
 }
 
 variable "contestant_team_ids" {
@@ -38,6 +43,7 @@ variable "contestant_team_ids" {
     takahashi  = "20"
     eagletmt   = "14"
     sapphi_red = "2"
+    sorah      = "15"
   }
 }
 
@@ -133,4 +139,51 @@ resource "aws_eip" "contestant-2" {
 
   vpc      = true
   instance = aws_instance.contestant-2[each.key].id
+}
+
+resource "aws_instance" "contestant-3" {
+  for_each = toset(var.three_instance_contestant_names)
+
+  #ami           = data.aws_ami.contestant.id
+  ami           = "ami-027107e4db237a066"
+  instance_type = "c5.large"
+
+  availability_zone = var.availability_zones[0]
+  subnet_id         = data.aws_subnet.public[0].id
+
+  vpc_security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.final-dev-contestant.id,
+  ]
+
+  tags = {
+    Name = "final-dev-contestant-${each.key}-3"
+
+    IsuconTeamID      = var.contestant_team_ids[each.key]
+    IsuconInstanceNum = "3"
+  }
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = "30"
+    tags = {
+      Name    = "final-dev-contestant-${each.key}-3"
+      Project = "final-dev"
+    }
+  }
+
+  user_data = file("${path.module}/contestant-user-data.sh")
+
+  lifecycle {
+    ignore_changes = [
+      ami,
+    ]
+  }
+}
+
+resource "aws_eip" "contestant-3" {
+  for_each = toset(var.three_instance_contestant_names)
+
+  vpc      = true
+  instance = aws_instance.contestant-3[each.key].id
 }
