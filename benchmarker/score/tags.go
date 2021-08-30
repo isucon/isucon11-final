6bc89fae110daacee66c543e35e4115522a21ac5
motoki317 <motoki317@gmail.com>
2021-08-30 18:58:05 +0900

    ♻️ split score files

--- benchmarker/score/tags.go
+++ benchmarker/score/tags.go
@@ -1,8 +1,6 @@
 package score
 
-import (
-	"github.com/isucon/isucandar/score"
-)
+import "github.com/isucon/isucandar/score"
 
 const (
 	CountRegisterCourses              score.ScoreTag = "01.RegisterCourses"
@@ -84,39 +82,4 @@ func maxLength(arr []score.ScoreTag) int {
 		}
 	}
 	return max
-}
-
-type mag int64      // 1回でn点
-type fraction int64 // n回で1点
-
-var scoreCoefTable = map[score.ScoreTag]interface{}{
-	CountRegisterCourses:  mag(10),
-	CountSubmitAssignment: mag(5),
-
-	CountGetGrades:           fraction(10),
-	CountGetAnnouncementList: fraction(10),
-}
-
-var (
-	deductionScore = int64(50) // エラーの減点スコア
-
-	// (timeoutDeductFraction)回あたり減点(timeoutDeductionScore)点
-	timeoutDeductionScore = int64(100) // タイムアウトの減点スコア
-	timeoutDeductFraction = int64(100) // タイムアウトで減点される回数
-)
-
-func Calc(result score.ScoreTable, deductionCount, timeoutCount int64) (totalScore, rawScore, deductScore int64) {
-	for tag, coefficient := range scoreCoefTable {
-		if mag, ok := coefficient.(mag); ok {
-			rawScore += result[tag] * int64(mag)
-		} else if fraction, ok := coefficient.(fraction); ok {
-			rawScore += result[tag] / int64(fraction)
-		}
-	}
-
-	deductScore += deductionCount * deductionScore
-	deductScore += (timeoutCount / timeoutDeductFraction) * timeoutDeductionScore
-
-	totalScore = rawScore - deductScore
-	return
 }
