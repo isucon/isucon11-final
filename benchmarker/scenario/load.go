b0baf275b114e4a2a49374718606eeb52537a8a9
yhattori <yhattori@yahoo-corp.jp>
2021-08-30 19:25:18 +0900

    [bench] retry submit

--- benchmarker/scenario/load.go
+++ benchmarker/scenario/load.go
@@ -738,7 +738,14 @@ func (s *Scenario) submitAssignments(ctx context.Context, students map[string]*m
 				return
 			}
 
+		L:
 			_, err = SubmitAssignmentAction(ctx, student.Agent, course.ID, class.ID, fileName, submissionData)
+			var urlError *url.Error
+			if errors.As(err, &urlError) && urlError.Timeout() {
+				ContestantLogger.Printf("課題提出(POST /:courseID/classes/:classID/assignment)がタイムアウトしました。学生はリトライを試みます。")
+				<-time.After(100 * time.Millisecond)
+				goto L
+			}
 			if err != nil {
 				step.AddError(err)
 			} else {
