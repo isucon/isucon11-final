ba493f2bd92423ce3b91a59063f12881a2f8fa89
TakahashiKazuya <kaz.t2497.bz@gmail.com>
2021-09-07 18:51:57 +0900

    :recycle: fix generate announcement

--- benchmarker/generate/announcement.go
+++ benchmarker/generate/announcement.go
@@ -2,24 +2,18 @@ package generate
 
 import (
 	"fmt"
-	"io"
 	"math/rand"
 	"time"
 
-	"github.com/isucon/isucon11-final/benchmarker/model"
 	"github.com/oklog/ulid/v2"
+
+	"github.com/isucon/isucon11-final/benchmarker/model"
 )
-
-var entropy io.Reader
-
-func init() {
-	entropy = rand.New(rand.NewSource(time.Now().UnixNano()))
-}
 
 func Announcement(course *model.Course, class *model.Class) *model.Announcement {
 	createdAt := GenTime()
 	return &model.Announcement{
-		ID:         ulid.MustNew(uint64(createdAt*1000), entropy).String(),
+		ID:         ulid.MustNew(uint64(createdAt*1000), ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)).String(),
 		CourseID:   course.ID,
 		CourseName: course.Name,
 		Title:      fmt.Sprintf("クラス追加: %s", class.Title),
