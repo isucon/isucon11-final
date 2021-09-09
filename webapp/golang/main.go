f7b763f8809b3e0d24c0ac4f088626e2fcc0cd5e
yhattori <yhattori@yahoo-corp.jp>
2021-09-09 18:34:46 +0900

    ♻️  separate init data

--- webapp/golang/main.go
+++ webapp/golang/main.go
@@ -99,6 +99,7 @@ func (h *handlers) Initialize(c echo.Context) error {
 	files := []string{
 		"1_schema.sql",
 		"2_init.sql",
+		"3_sample.sql",
 	}
 	for _, file := range files {
 		data, err := ioutil.ReadFile(SQLDirectory + file)
