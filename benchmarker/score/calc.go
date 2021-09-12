b5daae25ab40084bbcfc352c59f556d660e5cc0f
yhattori <yhattori@yahoo-corp.jp>
2021-09-12 23:38:29 +0900

    [bench] 🔧 fix scoring

--- benchmarker/score/calc.go
+++ benchmarker/score/calc.go
@@ -6,9 +6,8 @@ type mag int64      // 1回でn点
 type fraction int64 // n回で1点
 
 var scoreCoefTable = map[score.ScoreTag]interface{}{
-	ScoreSubmitAssignment: mag(2),
-
-	ScoreGetAnnouncementList: fraction(2),
+	ScoreSubmitAssignment:    mag(1),
+	ScoreGetAnnouncementList: mag(1),
 }
 
 var (
