52e90fb93b6bc4c80531d9274ed00c97ad3e5567
ryoha000 <the7uya@gmail.com>
2021-09-15 21:29:33 +0900

    一番外のCardのwidthを8/12に統一

--- webapp/frontend/pages/register.vue
+++ webapp/frontend/pages/register.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="py-10 px-8 bg-white shadow-lg mt-8 mb-8 rounded">
+    <div class="py-10 px-8 bg-white shadow-lg mt-8 mb-8 rounded w-8/12">
       <div class="flex-1 flex-col">
         <h1 class="text-2xl">履修登録</h1>
         <div class="flex mt-2 mb-6 gap-2">
